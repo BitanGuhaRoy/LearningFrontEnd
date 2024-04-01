b5cb8f86ca810fab11b9710d833646bb5a84594d
Bitan Guha Roy <bitanguharoy@gmail.com>
2024-04-01 17:48:04 +0530

    static method

--- Lec22(OOPS2)/class.js
+++ Lec22(OOPS2)/class.js
@@ -7,7 +7,7 @@ class Person
         console.log(`This is constructor`);
     }
 
-    isAdult()
+    isAdult() // this kind of method is called prototype method
     {
         if(this.age >= 18)
         {
@@ -18,7 +18,14 @@ class Person
             return  `${this.name} is not an adult`;
         }
     }
+
+    static staticMethod()
+    {
+        console.log(`This is static method`);
+    }
 }
 
 let person1 = new Person('Bitan', 25);
 console.log(person1.isAdult());
+
+Person.staticMethod();
