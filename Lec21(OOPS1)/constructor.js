f92d5e0c1ea14a0888b2a5abbf1b3635db026da2
Bitan Guha Roy <bitanguharoy@gmail.com>
2024-04-01 16:42:44 +0530

    good coding practice naming constructor function and class

--- Lec21(OOPS1)/constructor.js
+++ Lec21(OOPS1)/constructor.js
@@ -1,6 +1,6 @@
 // Template in JS that creates objects is called constructor 
 
-function car(_name, _color,_topspeed)
+function Car(_name, _color,_topspeed)
 {
     this.name= _name;
     this.color= _color;
@@ -13,8 +13,8 @@ function car(_name, _color,_topspeed)
 
 }
 
-let car1= new car('BMW', 'Black', 250);
-let car2= new car('Audi', 'White', 200);
-let car3= new car('Mercedes', 'Grey', 300);
+let car1= new Car('BMW', 'Black', 250);
+let car2= new Car('Audi', 'White', 200);
+let car3= new Car('Mercedes', 'Grey', 300);
 
 (car1.drive());
