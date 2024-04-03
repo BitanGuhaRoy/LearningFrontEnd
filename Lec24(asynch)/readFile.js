00576a105a41a3e02600fc512e6493f71815315b
Bitan Guha Roy <bitanguharoy@gmail.com>
2024-04-03 19:44:31 +0530

    Sync and Async file reads

---
+++ Lec24(asynch)/readFile.js
@@ -0,0 +1,53 @@
+const fs=require("fs");
+
+
+// synchronously reaidng the files
+console.log("before");
+
+let data1=fs.readFileSync("f1.txt")
+let data2=fs.readFileSync("f2.txt")
+let data3=fs.readFileSync("f3.txt")
+// console.log(data1+"");
+console.log(data2+"");
+console.log(data3+"");
+
+console.log("after");
+fs.readFile("f1.txt",fileRead);
+
+function fileRead(err, data)
+{
+   if(err)
+   {
+     console.log(err);
+   }
+   else {
+        console.log(data+"");
+   }
+}
+fs.readFile("f2.txt",fileRead2);
+
+function fileRead2(err, data)
+{
+   if(err)
+   {
+     console.log(err);
+   }
+   else {
+        console.log(data+"");
+   }
+}
+fs.readFile("f3.txt",fileRead3);
+
+function fileRead3(err, data)
+{
+   if(err)
+   {
+     console.log(err);
+   }
+   else {
+        console.log(data+"");
+   }
+}
+
+console.log("after async read file ");
+
