1fc80f3f9930264b991ca8b7b4979051d4d1f15b
Bitan Guha Roy <bitanguharoy@gmail.com>
2024-04-03 23:56:54 +0530

    readfile

--- Lec24(asynch)/readFile.js
+++ Lec24(asynch)/readFile.js
@@ -8,8 +8,8 @@ let data1=fs.readFileSync("f1.txt")
 let data2=fs.readFileSync("f2.txt")
 let data3=fs.readFileSync("f3.txt")
 // console.log(data1+"");
-console.log(data2+"");
-console.log(data3+"");
+// console.log(data2+"");
+// console.log(data3+"");
 
 console.log("after");
 fs.readFile("f1.txt",fileRead);
@@ -22,9 +22,11 @@ function fileRead(err, data)
    }
    else {
         console.log(data+"");
+        // fs.readFile("f2.txt",fileRead2); // This will make it in order 
    }
 }
 fs.readFile("f2.txt",fileRead2);
+// parallaly these callbacks are added into queue,so order is not maintained among callbacks 
 
 function fileRead2(err, data)
 {
@@ -34,6 +36,7 @@ function fileRead2(err, data)
    }
    else {
         console.log(data+"");
+        // fs.readFile("f3.txt",fileRead3); // This will make it in order
    }
 }
 fs.readFile("f3.txt",fileRead3);
